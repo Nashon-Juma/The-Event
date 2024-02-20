39b0ed773a3301123a058f51a58faafc6b36d4fc
Juma <nashonhhh@gmail.com>
2024-02-20 18:19:12 +0300

    woe

--- app/Http/Controllers/BuyController.php
+++ app/Http/Controllers/BuyController.php
@@ -2,22 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Customer;
-use app\Models\buy;
-
-use Illuminate\Http\Request;
+use app\Customer;
 
 
 class BuyController extends Controller
 {
     public function buy()
     {
-        $buyer= new Customer();
+        $buyer= new customer();
+
         $buyer->name =request('your_name');
         $buyer->email =request('your_email');
         $buyer->ticket =request('ticket_type');
 
+        $buyer->save();
+        
         return view('buy.thank');
-
     }
 }
