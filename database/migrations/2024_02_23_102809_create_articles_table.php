1d1faaf5944a70fe60e58e94f5b7001d17fd113a
Juma <nashonhhh@gmail.com>
2024-02-23 18:59:13 +0300

    none

--- database/migrations/2024_02_23_102809_create_articles_table.php
+++ database/migrations/2024_02_23_102809_create_articles_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
             $table->string('email');
+            $table->string('title');
             $table->string('description');
             $table->timestamps();
         });
