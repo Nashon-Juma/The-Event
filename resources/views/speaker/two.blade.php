c6190937d782f3a85cc74a651ef449fa24ab8761
juma <nashonhhh@gmail.com>
2024-03-05 17:48:59 +0300

    Update two.blade.php

--- resources/views/speaker/two.blade.php
+++ resources/views/speaker/two.blade.php
@@ -38,7 +38,7 @@
 
     </section>
 
-  </main><!-- End #main -->
+  </main><!-- hj End #main -->
 @endsection
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
