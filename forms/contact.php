7c4a2f83101fd230c5b5990613685d9479804b3f
juma <nashonhhh@gmail.com>
2024-03-23 21:13:05 +0300

    Pui

--- forms/contact.php
+++ forms/contact.php
@@ -32,9 +32,9 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact-> add_message( $_POST['name'], 'From');
+  $contact-> add_message( $_POST['email'], 'Email');
+  $contact-> add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
 ?>
