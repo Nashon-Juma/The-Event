cc6b35d1b9909e8100d150371ccdce437635c4cc
juma <nashonhhh@gmail.com>
2024-04-21 20:07:38 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@ use Symfony\Component\Mime\Email;
  * Requires the "PHP Email Form" library
  * The "PHP Email Form" library is available only in the pro version of the template
  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
- * For more info and help: https://bootstrapmade.com/php-email-form/
+ * For more info and help: https://bootstrapmade.com
  */
 
 // Replace contact@example.com with your real receiving email address
